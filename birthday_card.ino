1671258a0f2953800d33b5453bb30214eb481f0e
DabuXD <42313243+DabuXD@users.noreply.github.com>
2021-07-01 11:02:17 +0500

    Add files via upload

---
+++ birthday_card.ino
@@ -0,0 +1,50 @@
+int length = 28; // the number of notes
+char notes[] = "GGAGcB GGAGdc GGxecBA yyecdc";
+int beats[] = { 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8,8, 16, 1, 2,2,8,8,8,8,16, 1,2,2,8,8,8,16 };
+int tempo = 150;
+int buzzer =2;
+void setup() {
+  // put your setup code here, to run once:
+//Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//Serial.println(analogRead(knock));
+  pinMode(buzzer,INPUT);
+if(analogRead(1) > 100){
+  pinMode(buzzer,OUTPUT);
+  for (int i = 0; i < length; i++) {
+   if (notes[i] == ' ') {
+     delay(beats[i] * tempo); // rest
+   } else {
+     playNote(notes[i], beats[i] * tempo);
+   }
+   // pause between notes
+   delay(tempo);
+    }    
+  }
+}
+
+void playNote(char note, int duration) {
+char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B',  'c', 'd', 'e', 'f', 'g', 'a', 'b',  'x', 'y' };
+int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956, 834,  765,  593,  468,  346,  224,  655 , 715 };
+int SPEE = 5;
+// play the tone corresponding to the note name
+for (int i = 0; i < 17; i++) {
+
+   if (names[i] == note) {
+    int newduration = duration/SPEE;
+     playTone(tones[i], newduration);
+   }
+}
+}
+
+void playTone(int tone, int duration) {
+for (long i = 0; i < duration * 1000L; i += tone * 2) {
+   digitalWrite(buzzer, HIGH);
+   delayMicroseconds(tone);
+   digitalWrite(buzzer, LOW);
+   delayMicroseconds(tone);
+    }
+}
